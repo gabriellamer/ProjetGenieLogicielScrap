647e3ea0d79e1078c3316a47091f42fd44ab42bf
SimCitee <simon.gdupre@gmail.com>
2014-11-08 13:12:07 -0500

    Ajout classe Employe et Prepose

---
+++ src/ca/uqtr/gl/entities/Prepose.java
@@ -0,0 +1,18 @@
+package ca.uqtr.gl.entities;
+
+import java.util.Date;
+
+import ca.uqtr.gl.domain.RegistreClient;
+
+public class Prepose extends Employe {
+	
+	private RegistreClient registre;
+
+	public Prepose(RegistreClient registre, String nom, String prenom, Date dateNaissance,
+			Adresse adresse, int noEmploye, String titre) {
+		super(nom, prenom, dateNaissance, adresse, noEmploye, titre);
+		
+		this.registre = registre;
+	}
+
+}
